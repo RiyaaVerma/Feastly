143ffbc9f36ed848c923174aa551f6f6d040e21b
Riya Verma <riyaaverma2711@gmail.com>
2026-02-20 12:39:23 +0530

    Initial commit - Feastly project setup

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './config/db.js'
+import dotenv from "dotenv"
+import foodRouter from './routes/foodRoute.js'
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config'
+import cartRouter from './routes/cartRoute.js';
+import { placeOrder } from './controllers/orderController.js';
+
+dotenv.config();
+
+  //app config
+  const app=express();
+  const port=process.env.PORT || 4000;
+
+  //middleware
+  app.use(express.json());    
+  app.use(cors());
+
+  //db connection
+  connectDB();
+
+  //api endpoints
+  app.use("/api/food",foodRouter);
+  app.use("/images",express.static('uploads'));
+  app.use("/api/user",userRouter)
+  app.use("/api/cart",cartRouter)
+  app.use("/api/order",placeOrder)
+
+  app.get("/",(req,res)=>{
+    res.send('API Working');
+  });
+  app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`);
+  });
+
